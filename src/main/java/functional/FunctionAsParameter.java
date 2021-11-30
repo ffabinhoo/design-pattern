7a17f23c90512fddfd38ba86dd839db1b51a6f79
Fabio Costa <ffabinhoo@gmail.com>
2021-11-30 11:09:30 +1100

    function programmer as parameter

--- src/main/java/functional/FunctionAsParameter.java
+++ src/main/java/functional/FunctionAsParameter.java
@@ -1,6 +1,7 @@
 package functional;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class FunctionAsParameter {
     protected static class MyMath{
@@ -14,12 +15,24 @@ public class FunctionAsParameter {
         public static Integer combine(BiFunction<Integer, Integer, Integer> combineFunc){
             return combineFunc.apply(2,3);
         }
+
+        public static Function<Integer, Integer> createMultiplier(Integer x){
+            return (Integer y) -> x * y;
+        }
     }
 
     public static void main(String[] args) {
         System.out.println(MyMath.combine(MyMath::add));
         System.out.println(MyMath.combine(MyMath::subtract));
         System.out.println(MyMath.combine((x,y) -> (2 * x) + (2 * y)));
+        Function<Integer, Integer> timesTwo = MyMath.createMultiplier(2);
+        Function<Integer, Integer> timesTree = MyMath.createMultiplier(3);
+
+        System.out.println(timesTwo.apply(5));
+        System.out.println(timesTree.apply(5));
+
+
+
     }
 
 }
