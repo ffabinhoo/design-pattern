b01fa8457a7db73c9321b2c95c31ff567642d24e
fabio.silvadacosta@asx.com.au+1-at-288261749873 <fabio.silvadacosta@asx.com.au>
2024-06-06 13:13:13 +1000

    test git push

--- src/main/java/multithreading/CallableFutureExample.java
+++ src/main/java/multithreading/CallableFutureExample.java
@@ -15,7 +15,7 @@ public class CallableFutureExample {
             Future<Integer> future = service.submit(new Task());
             allFutures.add(future);
         }
-        System.out.println("finished all futures...");
+        System.out.println("finished all futures test...");
 
         for (int i = 0; i < allFutures.size(); i++) {
             try {
